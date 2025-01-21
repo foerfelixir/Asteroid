f1b9a82ec81ac92d9af75ba7555b52a69f13b57b
foerfelixir <foerfelixir@gmail.com>
2025-01-21 09:29:56 +0800

    added containers to Player class before game loop start

--- main.py
+++ main.py
@@ -13,14 +13,21 @@ def main():
 	clock = pygame.time.Clock()
 	dt=0
 	screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	Player.containers = (updatable, drawable)
+
 	gamer = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+	
 	while 1:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill("black")
-		gamer.update(dt)
-		gamer.draw(screen)
+		for item in updatable:
+			item.update(dt)
+		for drawing in drawable:
+			drawing.draw(screen)
 		pygame.display.flip()
 		dt = clock.tick(60)/1000
 
